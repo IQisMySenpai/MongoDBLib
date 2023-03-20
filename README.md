0d015018f519cfbeceb099f91673e14a56f5e492
Jannick <jannick.schroer@gmail.com>
2023-03-20 10:49:13 +0100

    * Fixed a bug, when passing in null to checkDataFormat * Formatted Documentation to the Standard * Added an Example to the README * Changed the Name to MongoDBLib

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# MongoLib
+# MongoDBLib
+## Table of Contents
+1. [About The Project](#about-the-project)
+    - [Built With](#built-with)
+2. [Getting Started](#getting-started)
+
+## About The Project
 A PHP Library to handle the connection to the MongoDB database. It also contains functions to perform common database operations, which were simplified to make them easier to use.
+
+### Built With
+* PHP 8.0
+
+## Getting Started
+I defined my config data in its own php file. The contents of the `config.php` file are as follows:
+```php
+<?php
+return [
+    'db_username'=> 'myUserName',
+    'db_password'=> 'mySecretPassword',
+    'db_domain'=> 'myDomain',
+    'db_name'=> 'databaseIWantToAccess'
+];
+```
+
+I can then use the library as follows:
+```php
+<?php
+require_once __DIR__ . '/includes/mongodb_lib.php';
+$config = require_once __DIR__ . '/includes/config.php';
+
+$mongo = new MongoDBLib($config);
+
+$result = $mongo->find('products');
+print_r($result);
+```
--- mongodb_lib.php
+++ mongodb_lib.php
@@ -6,10 +6,10 @@ require_once __DIR__ . '/vendor/autoload.php';
  * operations, which were simplified to make them easier to use.
  * @package MongoLib
  * @author Jannick Schröer
- * @version 1.0.0
+ * @version 1.0.1
  * @since 1.0.0
  */
-class MongoLib {
+class MongoDBLib {
     private string $username = ''; // The username to connect to the database.
     private string $password = ''; // The password to connect to the database.
     private string $domain = ''; // The domain to connect to the database.
@@ -20,17 +20,22 @@ class MongoLib {
 
     /**
      * MongoLib constructor.
+     * @param array|null $config Array with Configs for 'db_username', 'db_password', 'db_domain' and 'db_name'.
      */
-    function __construct() {
+    function __construct(array $config = null) {
+        if (is_array($config)) {
+            $this->username = $config['db_username'] ?? '';
+            $this->password = $config['db_password'] ?? '';
+            $this->domain = $config['db_domain'] ?? 'localhost';
+            $this->db_name = $config['db_name'];
+        }
         $this->connect();
     }
 
     /**
      * Connects to the database.
-     *
-     * @since 1.0.0
-     *
      * @return void The connection is stored in $this->client. As well as the database in $this->database.
+     * @since 1.0.0
      */
     function connect(): void {
         $this->client = new MongoDB\Client(
@@ -52,13 +57,11 @@ class MongoLib {
 
     /**
      * Finds documents in a collection.
-     *
-     * @since 1.0.0
-     *
      * @param $collection string The collection to search.
      * @param $filter array|string|null The filter to apply to the query.
      * @param $options array|string|null The options to apply to the query. Note: The typeMap has a default value.
      * @return array The documents found.
+     * @since 1.0.0
      */
     function find(string $collection, array|string $filter = null, array|string $options = null): array {
         $this->checkDataFormat($filter);
@@ -72,13 +75,11 @@ class MongoLib {
 
     /**
      * Counts the number of documents in a collection.
-     *
-     * @since 1.0.0
-     *
      * @param $collection string The collection to search.
      * @param $filter array|string|null The filter to apply to the query.
      * @param $options array|string|null The options to apply to the query.
      * @return int The number of documents found.
+     * @since 1.0.0
      */
     function count($collection, $filter = null, $options = null): int {
         $this->checkDataFormat($filter);
@@ -89,12 +90,10 @@ class MongoLib {
 
     /**
      * Inserts a single document into a collection.
-     *
-     * @since 1.0.0
-     *
      * @param $collection string The collection to insert into.
      * @param $document array|string The document to insert.
      * @return string The id of the inserted document.
+     * @since 1.0.0
      */
     function insertOne(string $collection, array|string $document): string {
         if (is_string($document)) {
@@ -107,12 +106,10 @@ class MongoLib {
 
     /**
      * Inserts multiple documents into a collection.
-     *
-     * @since 1.0.0
-     *
      * @param $collection string The collection to insert into.
      * @param $documents array The documents to insert.
      * @return array The ids of the inserted documents.
+     * @since 1.0.0
      */
     function insertMany(string $collection, array $documents): array {
         $results = $this->database->{$collection}->insertMany($documents);
@@ -121,12 +118,10 @@ class MongoLib {
 
     /**
      * Deletes a single document from a collection.
-     *
-     * @since 1.0.0
-     *
      * @param $collection string The collection to delete from.
      * @param $filter array|string The filter to apply to the query.
      * @return int The number of documents deleted.
+     * @since 1.0.0
      */
     function deleteOne(string $collection, array|string $filter): int {
         $this->checkDataFormat($filter);
@@ -136,12 +131,10 @@ class MongoLib {
 
     /**
      * Deletes multiple documents from a collection.
-     *
-     * @since 1.0.0
-     *
      * @param $collection string The collection to delete from.
      * @param $filter array|string The filter to apply to the query.
      * @return int The number of documents deleted.
+     * @since 1.0.0
      */
     function deleteMany(string $collection, array|string $filter): int {
         $this->checkDataFormat($filter);
@@ -151,14 +144,12 @@ class MongoLib {
 
     /**
      * Updates a single document in a collection.
-     *
-     * @since 1.0.0
-     *
      * @param $collection string The collection to update.
      * @param $filter array|string The filter to apply to the query.
      * @param $update array|string The update to apply to the document.
      * @param $options array|string|null The options to apply to the query.
      * @return int The number of documents updated.
+     * @since 1.0.0
      */
     function updateOne(string $collection, array|string$filter, array|string$update, array|string $options = null): int {
         $this->checkDataFormat($filter);
@@ -171,14 +162,12 @@ class MongoLib {
 
     /**
      * Updates multiple documents in a collection.
-     *
-     * @since 1.0.0
-     *
      * @param $collection string The collection to update.
      * @param $filter array|string The filter to apply to the query.
      * @param $update array|string The update to apply to the document.
      * @param $options array|string|null The options to apply to the query.
      * @return int The number of documents updated.
+     * @since 1.0.0
      */
     function updateMany(string $collection, array|string $filter, array|string $update, array|string $options = null): int {
         $this->checkDataFormat($filter);
@@ -191,12 +180,10 @@ class MongoLib {
 
     /**
      * Performs a bulk write operation.
-     *
-     * @since 1.0.0
-     *
      * @param $collection string The collection to update.
      * @param $operations array The operations to perform.
      * @param $options array|string|null The options to apply to the query.
+     * @since 1.0.0
      */
     function bulkWrite(string $collection, array $operations, array|string $options = null): int {
         $this->checkDataFormat($options);
@@ -207,11 +194,9 @@ class MongoLib {
 
     /**
      * Converts a string to a MongoDB\BSON\ObjectId.
-     *
-     * @since 1.0.0
-     *
      * @param $id string The string to convert.
      * @return \MongoDB\BSON\ObjectId The converted string.
+     * @since 1.0.0
      */
     static function stringToObjectId(string $id): MongoDB\BSON\ObjectId {
         return new MongoDB\BSON\ObjectId($id);
@@ -219,13 +204,11 @@ class MongoLib {
 
     /**
      * Checks if the data is a string and converts it to an array.
-     *
-     * @since 1.0.0
-     *
-     * @param $data array|string The data to check. This is passed by reference.
+     * @param array|string|null $data array|string The data to check. This is passed by reference.
      * @return void
+     * @since 1.0.0
      */
-    static function checkDataFormat(array|string &$data): void
+    static function checkDataFormat(array|string &$data = null): void
     {
         if (empty($data)) {
             $data = [];
@@ -236,11 +219,9 @@ class MongoLib {
 
     /**
      * Sets the typeMap to an array if it is not set.
-     *
-     * @since 1.0.0
-     *
      * @param $data array The data to check. This is passed by reference.
      * @return void
+     * @since 1.0.0
      */
     static function typeMapDefault(array &$data = []): void {
         if (!array_key_exists('typeMap', $data)) {
